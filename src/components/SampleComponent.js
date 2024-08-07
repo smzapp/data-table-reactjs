6cff70d97b2d32e9ae50de78293a57cbff7e7165
samuel <leumasrodama2009@gmail.com>
2024-08-07 16:28:24 +0800

    Update Samplecomponent

---
+++ src/components/SampleComponent.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function SampleComponent() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios.get('http://example.com/api/data')
+      .then(response => {
+          setData(response.data);
+      })
+      .catch(error => {
+          setError('An error occurred');
+      });
+  }, []);
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
